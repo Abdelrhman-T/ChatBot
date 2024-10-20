64a28e7dda78dac441e3d37b6e67f9f01afe59d5
Abdelrhman Tarek <Abdelrhmantarek2002@gmail.com>
2024-10-20 16:08:37 +0300

    ChatBotV3

--- app.py
+++ app.py
@@ -26,14 +26,26 @@ app = Flask(__name__)
 
 from tensorflow.keras.preprocessing.text import Tokenizer
 
-df_process = pd.read_csv("models/df_process.csv")
+df_process = pd.read_csv("models/df_processed.csv")
 
 tokenizer = Tokenizer()
 tokenizer.fit_on_texts(df_process["input_texts processed"]+df_process["target_texts processed"])
 
+
+from spacy.lang.en import stop_words
+stop_words = stop_words.STOP_WORDS
+
+not_ = ["not", "no", "nor", "n't", "really", "extremely", "very", "don't", "and", "or"]
+QUESTION_WORDS = ['what', 'why', 'how', 'who', 'where', 'when', 'which']
+not_remove = not_ + QUESTION_WORDS
+
+for word in not_remove:
+  if word in stop_words:
+    stop_words.remove(word)
+
 nlp = spacy.load("en_core_web_sm")
 
-max_sequence_len_encoder, max_sequence_len_decoder = 14, 61
+max_sequence_len_encoder, max_sequence_len_decoder = 17, 66
 total_words = len(tokenizer.word_index) + 1
 
 def convert_time_to_words(time_str):
@@ -43,16 +55,44 @@ def convert_time_to_words(time_str):
     time_in_words = f"{hours} {minutes}"
     return time_in_words
 
-def preprocess_text(text):
-    text = re.sub(r'\d{1,2}:\d{2}', lambda x: convert_time_to_words(x.group()), text)
-    text = re.sub(r"\d+", lambda x: num2words(int(x.group())), text)
+def preprocess_answer_text(text):
+    text = re.sub(r"&", "and", text)
+    text = re.sub(r"@\w+|http[^ ]+|#", '', text)
+    text = re.sub(r"[^\x00-\x7F]+", "'", text)
+    text = re.sub(r"[^\w\s.,_']", '', text)
+    text = re.sub(r"!=\s*", 'not ', text)
     text = re.sub(r"\s+", " ", text)
     text = text.strip()
     text = text.lower()
     return text
 
+def preprocess_question_text(text):
+    text = re.sub(r"&", "and", text)
+    text = re.sub(r'\bis\b', '', text)
+    text = re.sub(r"\\'", "'", text)
+    text = re.sub(r"\\[a-z]","",text)
+    text = re.sub(r"[.]{2,}","aaa", text)
+    text = re.sub(r"[.]"," ",text)
+    text = re.sub(r"[,]"," ",text)
+    text = re.sub(r" ' ","'",text)
+    text = re.sub(r'\d{1,2}:\d{2}', lambda x: convert_time_to_words(x.group()), text)
+    text = re.sub(r"\d+", lambda x: num2words(int(x.group())), text)
+    text = re.sub(r"\s+", " ", text)
+    text = text.strip()
+    text = text.lower()
+    doc = nlp(text)
+    keep_punctuation = {"(", ")"}
 
-seq2seq = load_model("models\chatBot_model.h5")
+    tokens = [token.lemma_ for token in doc if 
+              (token.text in keep_punctuation or not token.is_punct) and 
+              (token.text not in stop_words or token.pos_ == "VERB")]
+    text = " ".join(tokens)
+    text = re.sub(r"\s+", " ", text)
+    return text
+
+
+seq2seq = load_model("models/chatBotAI_model.h5")
+
 
 
 # Inference Encoder Model
@@ -84,22 +124,18 @@ decoder_model = Model([decoder_inputs] + decoder_state_inputs, [decoder_outputs]
 
 
 def chatBot(question):
-    # Preprocess the input text
-    input_text = preprocess_text(question)  # Assuming this is your custom preprocessing function
+    input_text = preprocess_question_text(question)
 
     # Tokenize and pad the input text
     input_sequence = tokenizer.texts_to_sequences([input_text])
     input_sequence = pad_sequences(input_sequence, maxlen=max_sequence_len_encoder, padding='post')
 
-    # Convert to TensorFlow tensor
     input_tensor = tf.convert_to_tensor(input_sequence)
 
-    # Get encoder states
     stat = encoder_model.predict(input_tensor, verbose=0)
 
-    # Prepare for decoding
     empty_target_seq = np.zeros((1, 1))
-    empty_target_seq[0, 0] = tokenizer.word_index.get("[CLS]", 1)  # Assuming [CLS] is the start token
+    empty_target_seq[0, 0] = tokenizer.word_index.get("[CLS]", 1) 
 
     stop_condition = False
     decoded_translation = ''
@@ -107,27 +143,24 @@ def chatBot(question):
     while not stop_condition:
         dec_outputs, h, c = decoder_model.predict([empty_target_seq] + stat, verbose=0)
 
-        # Get the index of the sampled word
         sampled_word_index = np.argmax(dec_outputs[0, -1, :])
-        sampled_word = tokenizer.index_word.get(sampled_word_index, "[UNK]")  # Convert index back to word
+        sampled_word = tokenizer.index_word.get(sampled_word_index, "[UNK]") 
 
-        if sampled_word in ['[SEP]', '[PAD]', '[UNK]'] or len(decoded_translation.split()) > max_sequence_len_decoder:
+        if sampled_word in ['[SEP]', '[PAD]', '[UNK]', 'sep'] or len(decoded_translation.split()) > max_sequence_len_decoder:
             stop_condition = True
 
-        if sampled_word not in ['[SEP]', '[PAD]', '[UNK]']:
+        if sampled_word not in ['[SEP]', '[PAD]', '[UNK]' , 'sep']:
             decoded_translation += sampled_word + ' '
 
-        # Prepare the next target sequence
         empty_target_seq = np.zeros((1, 1))
         empty_target_seq[0, 0] = sampled_word_index
 
-        # Update states
         stat = [h, c]
 
     return decoded_translation.strip()
 
 
-#-------------OUR------------------#
+#-------------Kayo------------------#
 def generate_answer_our(question):
     
     answer = chatBot(question)
@@ -136,7 +169,7 @@ def generate_answer_our(question):
 #-------------Groq-----------------#
 def generate_answer_groq(question):
     prompt = question
-    client = Groq(api_key='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')
+    client = Groq(api_key='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')
     chat_completion = client.chat.completions.create(
         messages=[{"role": "user", "content": prompt}],
         model="llama3-8b-8192",
@@ -165,10 +198,7 @@ def groq_answer():
     question = request.json['question']
     answer = generate_answer_groq(question)
     
-    result = text_analytics_clint.analyze_sentiment(documents=[question], show_opinion_mining=True)
-    doc = [res for res in result if not res.is_error]
-    
-    return jsonify({'answer': answer, 'sentiment': doc[0].sentiment})
+    return jsonify({'answer': answer})
 
 # New chatbot answer generation
 @app.route('/new_answer', methods=['POST'])
